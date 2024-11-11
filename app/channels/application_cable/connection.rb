94aa728b55e4766c5ff3544dc6408a4609272e75
Lucas Bertuleza <lucascbertuleza@gmail.com>
2024-11-11 20:59:59 +0000

    feat: configurar ActionCable para real-time

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,19 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+
+    def find_verified_user
+      if (verified_user = cookies.encrypted["_main_app_session"].dig("user", "email"))
+        verified_user
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
